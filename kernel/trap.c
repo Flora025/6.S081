1c62d2e643a68379a4d716871823a14e2815e6f0
Flora025 <2365349265@qq.com>
2023-09-30 18:17:38 -0400

    feat: lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,21 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13 || r_scause() == 15) {
+    // if pagefault
+    // get the page boundary
+    uint64 addr = PGROUNDDOWN(r_stval());
+    // alloc memory
+    char *mem = kalloc();
+    if(mem == 0){
+      printf("usertrap: failed to alloc mem\n");
+    }
+    memset(mem, 0, PGSIZE);
+    if(mappages(p->pagetable, addr, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+      kfree(mem);
+      printf("usertrap: failed to mappages\n");
+    }
+
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
